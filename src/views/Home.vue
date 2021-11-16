0757009c15694348d9670d915ae69127591dc448
Ian S <iansilber32@gmail.com>
2021-11-16 18:04:24 -0500

    fixes homework

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,31 +3,27 @@
     <div v-for="movie in movies" :key="movie.id">
       <h1>Title: {{ movie.title }}</h1>
       <h4>Plot: {{ movie.plot }}</h4>
-      <form v-on:submit.prevent="updateMovie()">
-        <h1>New Movie</h1>
-        <div>
-          <label>Title:</label>
-          <input type="text" v-model="movie.title" />
-        </div>
-        <div>
-          <label>Year:</label>
-          <input type="text" v-model="movie.year" />
-        </div>
-        <div>
-          <label>Plot:</label>
-          <input type="text" v-model="movie.plot" />
-        </div>
-        <div>
-          <label>Director:</label>
-          <input type="text" v-model="movie.director" />
-        </div>
-        <div>
-          <label>English (true or false):</label>
-          <input type="text" v-model="movie.english" />
-        </div>
-        <input type="submit" value="Update" />
-      </form>
-      <button v-on:click="deleteMovie()">Delete Movie</button>
+      <dialog id="movie-details">
+        <form method="dialog">
+          <h2>Movie Info:</h2>
+          <p>
+            Title:
+            <input type="text" v-model="currentMovie.title" />
+          </p>
+          <p>
+            Plot:
+            <input type="text" v-model="currentMovie.plot" />
+          </p>
+          <p>
+            Year:
+            <input type="integer" v-model="currentMovie.year" />
+          </p>
+          <button v-on:click="updateMovie(currentMovie)">Update Movie</button>
+          <button v-on:click="deleteMovie(currentMovie)">Delete Movie</button>
+          <button>Close</button>
+        </form>
+      </dialog>
+      <button v-on:click="showMovie(movie)">More Info</button>
     </div>
     <form v-on:submit.prevent="createMovie()">
       <h1>New Movie</h1>
@@ -64,6 +60,7 @@ export default {
     return {
       movies: [],
       newMovieParams: {},
+      currentMovie: {},
     };
   },
   created: function () {
@@ -75,7 +72,7 @@ export default {
         .get("/movies")
         .then((response) => {
           this.movies = response.data;
-          console.log("Success!", this.movies);
+          console.log("Successfully indexed movies!", this.movies);
         })
         .catch((error) => console.log(error.response));
     },
@@ -87,19 +84,25 @@ export default {
         })
         .catch((error) => console.log(error.response));
     },
-    updateMovie: function () {
+    updateMovie: function (movie) {
       axios
-        .patch(`/movies${this.$route.params.id}`, this.newMovieParams)
-        .then(() => {
+        .patch("/movies/" + movie.id, movie)
+        .then((response) => {
+          console.log("Successfully updated movie!", response.data);
           this.$router.push("/success");
         })
         .catch((error) => console.log(error.response));
     },
-    deleteMovie: function () {
-      axios.delete(`/movies/${this.$route.params.id}`).then((response) => {
+    deleteMovie: function (movie) {
+      axios.delete("/movies/" + movie.id).then((response) => {
         console.log("DELETED", response.data);
         this.$router.push("/success");
       });
+    },
+    showMovie: function (movie) {
+      this.currentMovie = movie;
+      console.log(movie);
+      document.querySelector("#movie-details").showModal();
     },
   },
 };
